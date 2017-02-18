6971b149148e1842d36343e7c11cf6bd56801a1a
Matzefication <mathiaskaniut@Mathiass-Air.fritz.box>
2017-02-18 04:16:30 +0100

    Sensor integration

--- MMM-Move.js
+++ MMM-Move.js
@@ -6,13 +6,44 @@
  */
 
 Module.register("MMM-Move", {
-	defaults : {
-        text: "Hello world!"
-	},
-	  
+    // Define module defaults
+	defaults: {
+		echoPin: "24", //Echo pin
+		triggerPin: "18", //Trigger pin
+		sensorTimeout: 500, //Measurement timeout in µs
+		interval: 300, //Measurement intervall
+        distance: 10 // Distance in cm
+	}, 
+
+    // Override start method.
+    start: function() {
+        var displayData = null;
+        
+        Log.log("Starting module: " + this.name);
+        
+        setInterval(function () {
+            this.sendSocketNotification('MESSUREMENT', this.config);
+        }, this.config.interval);
+    },
+    
+    // Override socket notification handler.
+	socketNotificationReceived: function (notification, payload) {
+        if ( notification === 'MESSURED') {
+            this.displayData = payload;
+			this.updateDom(this.config.animationSpeed);
+            if ( payload <= this.config.distance ) {
+                this.sendNotification('DISTANCE', payload);
+            }
+        }
+    },
+    
+    // Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
-        wrapper.innerHTML = this.config.text;
+		if(this.displayData !== undefined) {
+            wrapper.innerHTML = this.displayData;
+            wrapper.className = "dimmed light small";
+		}
 		return wrapper;
 	}
 });
---
+++ node_helper.js
@@ -0,0 +1,38 @@
+/* Magic Mirror
+ * Module: MMM-Move
+ * 
+ * By Mathias Kaniut
+ * MIT Licensed
+ */
+
+var NodeHelper = require("node_helper");
+var statistics = require('math-statistics');
+var usonic = require('r-pi-usonic');
+
+module.exports = NodeHelper.create({
+	// Override start method.
+	start: function() {
+		console.log("Starting node helper for: " + this.name);
+
+		usonic.init(function (error) {
+			if (error) {
+				console.log(error);
+			} else {
+				this.initSensor();
+            }
+		});
+    },
+    
+    socketNotificationReceived: function (notification, payload) {
+        this.config = payload;
+        var sensor;
+        
+        if ( notification === 'MESSUREMENT') {
+            this.sensor = usonic.createSensor(this.config.echoPin, this.config.triggerPin, this.config.sensorTimeout);
+			this.sendSocketNotification('MESSURED', this.sensor().toFixed(2));        
+        }
+    },
+    
+	initSensor: function () {
+	}    
+});
